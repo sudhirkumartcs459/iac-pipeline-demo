2e8aad62893a7f596a36c0964df2856ec884da14
Cloud Logic <cloudlogic20@gmail.com>
2021-01-23 20:21:20 +0530

    added tfvars

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ data "aws_ami" "web_ami" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.web_ami.id
-  instance_type = "var.instacetype"
+  instance_type = var.instancetype
 
   tags = {
     Name = "HelloWorld-KR"
