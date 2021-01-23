0a97cbcf81d74bf7308594e2159fb03ec0e8c81f
Cloud Logic <cloudlogic20@gmail.com>
2021-01-23 22:24:29 +0530

    added tfvars

--- main.tf
+++ main.tf
@@ -50,3 +50,21 @@ resource "aws_security_group" "allow_http" {
     Name = "allow_http"
   }
 }
+
+data "aws_ssm_parameter" "dummyref" {
+  name = "secretdemo"
+}
+
+resource "aws_db_instance" "default" {
+  identifier           = "demo-rds"
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = data.aws_ssm_parameter.dummyref.value
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = "true"
+}
