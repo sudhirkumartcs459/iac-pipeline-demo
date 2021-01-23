ae8f9034130597562c77446584a3aa3a71cfd752
Cloud Logic <cloudlogic20@gmail.com>
2021-01-23 17:43:53 +0530

    updated template

--- modules/ec2/ec2.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_instance" "demoec2" {
-  ami           = "ami-04b1ddd35fd71475a"
-  instance_type = var.instance_type
-}
-
--- modules/iam/iam.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_iam_user" "demouser" {
-  name = var.username
-  path = "/users/"
-}
